22055cd43c728c5d66bf588bd9a01ebfb42d2f6f
Dhanoop Bhaskar <dhanoopbhaskar@gmail.com>
2014-08-13 22:21:26 +0530

    Added parity bit while generating the key.

--- src/DESkeygen.java
+++ src/DESkeygen.java
@@ -1,26 +1,61 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.nio.ByteBuffer;
 
 public class DESkeygen {
-	
+
 	private final File file = new File("DESkey.txt");
-	private final int KEY_LENGTH = 16;
+	private final int KEY_LENGTH = (16 * 8);
+	private final static char[] binArray = "01".toCharArray();
 	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-	
+
 	public DESkeygen() {
-		try {			
+		try {
 			FileWriter fileWriter = new FileWriter(file);
 			fileWriter.write("");
-			for (int i = 0 ; i < KEY_LENGTH ; i++) {
-				double random = Math.random();
-				int index = (int) (random * 16);
-				fileWriter.append(hexArray[index]);
-				fileWriter.flush();
+			StringBuffer buffer = new StringBuffer();			
+			for (int i = 0; i < KEY_LENGTH; i++) {
+				if ((i + 1) % 8 == 0) {
+					char parity = parityBit(new String(buffer));
+					buffer.append(parity);		
+					//System.out.println(buffer);					
+					fileWriter.append(bitsToHex(new String(buffer)));					
+					fileWriter.flush();
+					buffer = new StringBuffer();
+				} else {
+					double random = Math.random();
+					int index = (int) (random * 2);
+					buffer.append(binArray[index]);
+				}
 			}
 			fileWriter.close();
 			System.out.println("Key generated and saved in " + file.getName());
-		} catch(Exception exp) {
+		} catch (Exception exp) {
 			exp.printStackTrace();
+		}
+	}
+
+	private String bitsToHex(String input) {
+		String output = "";
+		int v = Integer.parseInt(input.substring(0, 4), 2);		
+		output += hexArray[v];
+		v = Integer.parseInt(input.substring(4, 8), 2);		
+		output += hexArray[v];		
+		return output;
+	}
+
+	private char parityBit(String input) {
+		char[] bits = input.toCharArray();
+		int count = 0;
+		for (int i = 0; i < bits.length; i++) {
+			if (bits[i] == '1') {
+				count++;
+			}
+		}
+		if (count % 2 == 0) {
+			return '0';
+		} else {
+			return '1';
 		}
 	}
 	
